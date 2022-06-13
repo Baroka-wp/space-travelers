6f36a7b357bb6249ff3a080cc09a5025cd38ff2c
baroka <birotori@gmail.com>
2022-06-13 14:49:30 +0100

    Display reserved rockets in My Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import ProfilRocketCard from './pages/ProfilRocketCard';
 
-const Profile = () => (
-  <div>
-    <h2>MY PROFILE</h2>
-  </div>
-);
+const Profile = () => {
+  const rockets = useSelector((state) => state.rockets);
+  const myRockets = rockets.filter((rocket) => rocket.reserved === true);
+  return (
+    <div>
+      <h2>My Rockets</h2>
+      <ProfilRocketCard myRockets={myRockets} />
+      <h2>My Missions</h2>
+    </div>
+
+  );
+};
 
 export default Profile;
---
+++ src/components/pages/ProfilRocketCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfilRocketCard = ({ myRockets }) => (
+  <table>
+    <tbody>
+      {
+        myRockets.map((rocket) => (
+          <tr key={rocket.id}>
+            <td>{rocket.rocket_name}</td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
+);
+
+ProfilRocketCard.propTypes = {
+  myRockets: PropTypes.shape({
+    map: PropTypes.func,
+    id: PropTypes.number,
+    rocket_name: PropTypes.string,
+  }).isRequired,
+};
+
+export default ProfilRocketCard;
