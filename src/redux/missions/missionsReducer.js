a4d07b9ffe1b46d009be4e38b21f1c3c7511b5d9
Lembani Sakala <lembanisakala@gmail.com>
2022-06-13 15:15:41 +0200

    Add `toggleMission` action to change status

--- src/redux/missions/missionsReducer.js
+++ src/redux/missions/missionsReducer.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 const GET_MISSIONS = 'space-travelers/redux/GET_MISSIONS';
+const TOGGLE_MISSION = 'space-travelers/redux/TOGGLE_MISSION';
+
 const url = 'https://api.spacexdata.com/v3/missions';
 
 const getMissions = (payload) => ({ type: GET_MISSIONS, payload });
+export const toggleMission = (payload) => ({ type: TOGGLE_MISSION, payload });
 
 export const fetchMissions = () => async (dispatch) => {
   const response = await axios.get(url);
@@ -21,10 +24,20 @@ export const fetchMissions = () => async (dispatch) => {
   dispatch(getMissions(missions));
 };
 
+const changeMissionStatus = (state, id) => {
+  const newState = state.map((mission) => {
+    if (mission.mission_id !== id) return mission;
+    return { ...mission, reserved: !mission.reserved };
+  });
+  return newState;
+};
+
 const missionsReducer = (state = [], action) => {
   switch (action.type) {
     case GET_MISSIONS:
       return action.payload;
+    case TOGGLE_MISSION:
+      return changeMissionStatus(state, action.payload);
     default:
       return state;
   }
