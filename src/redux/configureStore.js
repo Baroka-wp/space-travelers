74f680024b6972daa07eb210fb6f5d75951284d8
Lembani Sakala <lembanisakala@gmail.com>
2022-06-13 12:57:25 +0200

    Update configure to combine reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import missionsReducer from './missions/missionsReducer';
 import rocketsReducer from './rockets/rocketsReducer';
 
 const rootReducer = combineReducers({
   rockets: rocketsReducer,
+  missions: missionsReducer,
 });
 
 const store = configureStore({ reducer: rootReducer });
