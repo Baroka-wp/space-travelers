d1c46f68833f3eca09f4f2ea0df5385089451bed
Lembani Sakala <lembanisakala@gmail.com>
2022-06-13 16:09:38 +0200

    Create `ProfileMissionCard`

---
+++ src/components/pages/ProfileMissionCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileMissionCard = ({ myMissions }) => (
+  <table>
+    <tbody>
+      {
+        myMissions.map((mission) => (
+          <tr key={mission.mission_id}>
+            <td>{mission.mission_name}</td>
+          </tr>
+        ))
+      }
+    </tbody>
+  </table>
+);
+
+ProfileMissionCard.propTypes = {
+  myMissions: PropTypes.shape({
+    map: PropTypes.func,
+    mission_id: PropTypes.string,
+    mission_name: PropTypes.string,
+  }).isRequired,
+};
+
+export default ProfileMissionCard;
